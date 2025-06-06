a8025886029a7987eb3fd4196d94de51cbff54e0
xlehieu <xlehieu@gmail.com>
2025-06-06 17:43:28 +0700

    redeploy

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -11,17 +11,23 @@ export interface IUser {
     password?: string;
     email?: string;
 }
-const UserSchema = new mongoose.Schema<IUser>({
-    member_id: String,
-    domain: String,
-    access_token: String,
-    refresh_token: String,
-    expires_in: Number,
-    obtained_at: Number,
-    name: String,
-    password: String,
-    email: String,
-});
+const UserSchema = new mongoose.Schema<IUser>(
+    {
+        member_id: String,
+        domain: String,
+        access_token: String,
+        refresh_token: String,
+        expires_in: Number,
+        obtained_at: Number,
+        name: String,
+        password: String,
+        email: String,
+    },
+    {
+        timestamps: true, // Tự động thêm createdAt và updatedAt
+        bufferCommands: true,
+    },
+);
 
 const UserModel = mongoose.model<IUser>('User', UserSchema);
 export default UserModel;
